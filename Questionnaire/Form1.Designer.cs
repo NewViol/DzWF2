b60df2626d73761e68a7b0cf7025fe124e2d0ce3
NewViol <adrozd556@gmail.com>
2021-12-20 23:19:29 +0200

    new

---
+++ Questionnaire/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace Questionnaire
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxIm = new System.Windows.Forms.TextBox();
+            this.textBoxTel = new System.Windows.Forms.TextBox();
+            this.textBoxMail = new System.Windows.Forms.TextBox();
+            this.textBoxFam = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SaveBt = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.btEditing = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxIm
+            // 
+            this.textBoxIm.Location = new System.Drawing.Point(66, 32);
+            this.textBoxIm.Name = "textBoxIm";
+            this.textBoxIm.Size = new System.Drawing.Size(372, 22);
+            this.textBoxIm.TabIndex = 0;
+            // 
+            // textBoxTel
+            // 
+            this.textBoxTel.Location = new System.Drawing.Point(66, 218);
+            this.textBoxTel.Name = "textBoxTel";
+            this.textBoxTel.Size = new System.Drawing.Size(372, 22);
+            this.textBoxTel.TabIndex = 1;
+            // 
+            // textBoxMail
+            // 
+            this.textBoxMail.Location = new System.Drawing.Point(66, 155);
+            this.textBoxMail.Name = "textBoxMail";
+            this.textBoxMail.Size = new System.Drawing.Size(372, 22);
+            this.textBoxMail.TabIndex = 2;
+            // 
+            // textBoxFam
+            // 
+            this.textBoxFam.Location = new System.Drawing.Point(66, 93);
+            this.textBoxFam.Name = "textBoxFam";
+            this.textBoxFam.Size = new System.Drawing.Size(372, 22);
+            this.textBoxFam.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Number of telephone";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "e-mail";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Surname";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Name";
+            // 
+            // SaveBt
+            // 
+            this.SaveBt.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.SaveBt.Location = new System.Drawing.Point(66, 280);
+            this.SaveBt.Name = "SaveBt";
+            this.SaveBt.Size = new System.Drawing.Size(75, 23);
+            this.SaveBt.TabIndex = 8;
+            this.SaveBt.Text = "Save";
+            this.SaveBt.UseVisualStyleBackColor = true;
+            this.SaveBt.Click += new System.EventHandler(this.SaveBt_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(528, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 212);
+            this.listBox1.TabIndex = 9;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(193, 280);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 10;
+            this.Clear.Text = "Clear all";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btEditing
+            // 
+            this.btEditing.Location = new System.Drawing.Point(331, 280);
+            this.btEditing.Name = "btEditing";
+            this.btEditing.Size = new System.Drawing.Size(75, 23);
+            this.btEditing.TabIndex = 11;
+            this.btEditing.Text = "Editing";
+            this.btEditing.UseVisualStyleBackColor = true;
+            this.btEditing.Click += new System.EventHandler(this.btEditing_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 336);
+            this.Controls.Add(this.btEditing);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.SaveBt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxFam);
+            this.Controls.Add(this.textBoxMail);
+            this.Controls.Add(this.textBoxTel);
+            this.Controls.Add(this.textBoxIm);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxIm;
+        private System.Windows.Forms.TextBox textBoxTel;
+        private System.Windows.Forms.TextBox textBoxMail;
+        private System.Windows.Forms.TextBox textBoxFam;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button SaveBt;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button btEditing;
+    }
+}
+
---
+++ Questionnaire/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Questionnaire
+{
+    public partial class Form1 : Form
+    {
+
+        List<User> users = new List<User>();
+        bool IsEditing = false;
+        User TmpUser = new User();
+        public Form1()
+        {
+            InitializeComponent();
+            ReadToFile();
+        }
+
+        private void SaveBt_Click(object sender, EventArgs e)
+        {
+            User DopUser = new User();
+
+            bool IsIncorrect = true;
+
+            if (textBoxIm.Text != null)
+            {
+                if (textBoxFam.Text != null)
+                {
+                    if(textBoxMail.Text != null)
+                    {
+                        bool dop = false;
+                        for (int i =0;i < textBoxMail.Text.Length; i++)
+                        {
+                            if(textBoxMail.Text[i] == '@')
+                            {
+                                dop = true;
+                            }
+                        }
+                        if(dop == false)
+                        {
+                            MessageBox.Show("Incorrect input");
+                            IsIncorrect = false;
+                        }
+                        if(dop == true)
+                        {
+                            if (textBoxTel.Text != null)
+                            {
+                                bool isletter = false;
+                                if (textBoxTel.Text.Any(c => char.IsLetter(c)))
+                                {
+                                    isletter = true;
+                                }
+                                if(isletter == true)
+                                {
+                                    MessageBox.Show("Incorrect input");
+                                    IsIncorrect = false;
+                                }
+                                if(textBoxTel.Text.Length != 10)
+                                {
+                                    MessageBox.Show("Incorrect input");
+                                    IsIncorrect = false;
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Incorrect input");
+                                IsIncorrect = false;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Incorrect input");
+                        IsIncorrect = false;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect input");
+                    IsIncorrect = false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Incorrect input");
+                IsIncorrect = false;
+            }
+
+            if (IsIncorrect == true)
+            {
+                DopUser.Name = textBoxIm.Text;
+                DopUser.SurName = textBoxFam.Text;
+                DopUser.mail = textBoxMail.Text;
+                DopUser.phone = textBoxTel.Text;
+                users.Add(DopUser);
+                listBox1.Items.Add(DopUser);
+                listBox1.ValueMember = "SurName";
+                listBox1.DisplayMember = "SurName";
+                textBoxIm.Clear();
+                textBoxMail.Clear();
+                textBoxFam.Clear();
+                textBoxTel.Clear();
+
+                SaveToFile();
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            var user = (User)listBox1.SelectedItem;
+            textBoxIm.Text = user.Name;
+            textBoxFam.Text = user.SurName;
+            textBoxMail.Text = user.mail;
+            textBoxTel.Text = user.phone;
+            IsEditing = true;
+            TmpUser.Name = textBoxIm.Text;
+            TmpUser.SurName = textBoxFam.Text;
+            TmpUser.mail = textBoxMail.Text;
+            TmpUser.phone = textBoxTel.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxIm.Clear();
+            textBoxMail.Clear();
+            textBoxFam.Clear();
+            textBoxTel.Clear();
+        }
+        
+        private void SaveToFile()
+        {
+
+         
+
+            StreamWriter sw = new StreamWriter("Users.txt", false);
+
+
+            foreach (var i in users)
+            {
+                sw.WriteLine(i.Name);
+                sw.WriteLine(i.SurName);
+                sw.WriteLine(i.mail);
+                sw.WriteLine(i.phone);
+            }
+            sw.Close();
+        }
+        private void ReadToFile()
+        {
+            
+              FileStream file = new FileStream("Users.txt", FileMode.Open);
+              StreamReader rf = new StreamReader(file);
+            while (!rf.EndOfStream)
+            {
+                User DopUser = new User();
+                DopUser.Name = rf.ReadLine();
+                DopUser.SurName = rf.ReadLine();
+                DopUser.mail = rf.ReadLine();
+                DopUser.phone = rf.ReadLine();
+
+                users.Add(DopUser);
+            }
+              rf.Close();
+            foreach (var i in users)
+            {
+                listBox1.Items.Add(i);
+                listBox1.ValueMember = "SurName";
+                listBox1.DisplayMember = "SurName";                
+            }
+
+        }
+
+
+            private void btEditing_Click(object sender, EventArgs e)
+        {
+            if ((textBoxIm.Text != null) && (textBoxFam.Text != null) && (textBoxMail.Text != null) && (textBoxTel.Text != null))
+            {
+                if (IsEditing == true)
+                {
+                    for(int i=0;i < users.Count; i++)
+                    {
+                        if ((users[i].Name == TmpUser.Name) &&
+                            (users[i].SurName == TmpUser.SurName) &&
+                            (users[i].mail == TmpUser.mail) &&
+                            (users[i].phone == TmpUser.phone))
+                        {
+                            users[i].Name = textBoxIm.Text;
+                            users[i].SurName = textBoxFam.Text;
+                            users[i].mail = textBoxMail.Text;
+                            users[i].phone = textBoxTel.Text;
+                        }
+                    }
+                }
+            }
+            textBoxIm.Clear();
+            textBoxMail.Clear();
+            textBoxFam.Clear();
+            textBoxTel.Clear();
+        }
+    }
+}
