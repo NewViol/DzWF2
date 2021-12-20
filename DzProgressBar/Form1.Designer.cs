980d3082cc06a68ca940ed6e49e656f431b35027
NewViol <adrozd556@gmail.com>
2021-12-20 23:18:22 +0200

    new

---
+++ DzProgressBar/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace DzProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.buttonDownload = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(22, 229);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(476, 26);
+            this.progressBar.TabIndex = 0;
+            // 
+            // buttonDownload
+            // 
+            this.buttonDownload.Location = new System.Drawing.Point(504, 229);
+            this.buttonDownload.Name = "buttonDownload";
+            this.buttonDownload.Size = new System.Drawing.Size(91, 26);
+            this.buttonDownload.TabIndex = 1;
+            this.buttonDownload.Text = "Download";
+            this.buttonDownload.UseVisualStyleBackColor = true;
+            this.buttonDownload.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(22, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(573, 211);
+            this.textBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 280);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.buttonDownload);
+            this.Controls.Add(this.progressBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button buttonDownload;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ DzProgressBar/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Windows.Forms;
+
+namespace DzProgressBar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            VScrollBar dop = new VScrollBar();
+
+
+           // textBox.Controls.Add(dop);
+            textBox.ScrollBars = ScrollBars.Vertical;
+            dop.Scroll += progressScroll;
+
+            dop.MouseWheel += progressScrollForMouse;
+        }
+
+        private void progressScroll(object sender, ScrollEventArgs e)
+        {
+            var scroll = (VScrollBar)sender;
+            progressBar.Minimum = 0;
+            progressBar.Maximum = scroll.Maximum + 1;
+            progressBar.Value = scroll.Value;
+            progressBar.Step = scroll.Value / 10;
+            
+            if(scroll.Value != scroll.Maximum)
+            {
+                progressBar.PerformStep();
+            }
+        }
+
+        private void progressScrollForMouse(object sender, MouseEventArgs e)
+        {
+            var scroll = (VScrollBar)sender;
+            progressBar.Minimum = 0;
+            progressBar.Maximum = scroll.Maximum + 1;
+            progressBar.Value = scroll.Value;
+            progressBar.Step = scroll.Value / 10;
+
+            if (scroll.Value != scroll.Maximum)
+            {
+                progressBar.PerformStep();
+            }
+        }
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            FileStream file = new FileStream("DownloadText.txt", FileMode.OpenOrCreate);
+            string dopstr = "The agency said its verification of the Verkhoyansk record highlighted how temperatures were increasing in a climatically important region of the world.";            
+            
+            StreamWriter sw = new StreamWriter(file);
+
+            sw.Write(dopstr, dopstr.Length);
+            StreamReader sr = new StreamReader(file);
+
+            int sr_lenght = (int)file.Length;
+
+
+            string dopStr = string.Empty;
+            while (!sr.EndOfStream){
+                dopStr += sr.ReadLine();
+                progressBar.PerformStep();
+            }
+            sr.Close();
+            textBox.Text = dopStr;
+        }
+
+
+    }
+}
